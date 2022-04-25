8dfa68a814f1a2bfe0e554e72c2c4608f8154d6c
toshikazu.muto <muto@takadass.cp.jp>
2022-04-25 07:20:25 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         @include('commons.navbar')
 
         <div class="container">
+            {{-- エラーメッセージ --}}
+            @include('commons.error_tasks')
+
             @yield('content')
         </div>
 
