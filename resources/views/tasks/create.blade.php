22b54a96f8689f0a953ed9c204407cafe98dbe92
toshikazu.muto <muto@takadass.cp.jp>
2022-04-26 07:56:04 +0000

    task Procfile

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>メッセージ新規作成ページ</h1>
+    <h1>タスク新規作成ページ</h1>
 
     <div class="row">
         <div class="col-6">
@@ -14,7 +14,7 @@
                 </div>
 
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
 
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>メッセージ一覧</h1>
+    <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
         <table class="table table-striped">
@@ -10,13 +10,13 @@
                 <tr>
                     <th>id</th>
                     <th>ステータス</th>
-                    <th>メッセージ</th>
+                    <th>タスク</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    {{-- メッセージ詳細ページへのリンク --}}
+                    {{-- タスク詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
@@ -26,7 +26,7 @@
         </table>
     @endif
     
-    {{-- メッセージ作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
+    {{-- タスク作成ページへのリンク --}}
+    {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
